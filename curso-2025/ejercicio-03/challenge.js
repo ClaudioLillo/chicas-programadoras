d73f05b7c267df0a1dfec8985b373384a97c08da
Cristóbal Sepúlveda <cristobalsepulveda1@hotmail.com>
2025-04-01 22:14:28 -0300

    exercise: add exercice 03.

---
+++ curso-2025/ejercicio-03/challenge.js
@@ -0,0 +1,19 @@
+/**
+	Retorne un objeto organizando las personas por edad
+	la llave sera la edad y el valor un array con las personas.
+*/
+function organizeByAge(list) {}
+
+/**
+	Retorne un objeto organizando las personas por hobby
+	la llave sera el hobby y el valor un array de personas.
+*/
+function organizeByHobby(list) {}
+
+/**
+	Retorne un objeto con la suma de la edad de las personas
+	que tienen la misma edad.
+*/
+function sumByAge(list) {}
+
+export { organizeByAge, organizeByHobby, sumByAge };
